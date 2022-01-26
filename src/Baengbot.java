e697467e103ce21b125afe89561d1364226f247a
danbcooper <daniel.losenfeld@rwth-aachen.de>
2022-01-26 19:50:00 +0100

    New class

---
+++ src/Baengbot.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.regex.*;
+
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.image.*;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.*;
+
+import java.awt.event.KeyEvent;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.sax.BodyContentHandler;
+
+public class Baengbot{
+  Robot robot = new Robot();
+  String pdfContent = "";
+  
+  // Constructor
+  public Baengbot() throws AWTException, IOException, TikaException {
+  return;
+  }
+
+  public void run(Strung pdfPath) {
+
+  }
+}
